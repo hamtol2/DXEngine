afd5818df63105aebae8472aab615d796e72fb88
RonnieJ <ronniej@naver.com>
2019-07-08 17:44:26 +0900

    Day5 - Material
    - Material에 VS / PS 묶음.

---
+++ DXEngine12_Material/Engine/Engine.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "DXApp.h"
+#include "Mesh.h"
+#include "Material.h"
+//#include "VertexShader.h"
+//#include "PixelShader.h"
+
+class Engine : public DXApp
+{
+private:
+	struct PerSceneBuffer
+	{
+		PerSceneBuffer() { ZeroMemory(this, sizeof(this)); }
+
+		XMMATRIX viewProjection;
+		//XMMATRIX projection;
+		XMFLOAT3 worldLightPosition;
+		XMFLOAT3 worldCameraPosition;
+	};
+
+public:
+	Engine(HINSTANCE hinstance);
+	~Engine();
+
+	int Run();
+	bool Init() override;
+	void Update() override;
+	void Render() override;
+
+private:
+
+	// 장면(Scene) 초기화.
+	bool InitializeScene();
+	bool InitializeTransformation();
+
+private:
+
+	Material* material;
+	//VertexShader* vertexShader;
+	//PixelShader* pixelShader;
+
+	ID3D11Buffer* constantBuffer;			// 뷰, 투영행렬 버퍼.
+
+	// 카메라 정보.
+	XMVECTOR cameraPosition;		// 카메라 위치.
+	XMVECTOR cameraView;			// 카메라 방향.
+	XMVECTOR camerUpVector;			// 카메라 위 방향.
+
+	Mesh* mesh;
+};
